5ce0bc0569832a13f2750b8885bfee24d54e55a5
chintan <chintan.sakhiya@improwised.com>
2023-03-30 15:06:30 +0530

    create table using create element

--- data.js
+++ data.js
@@ -1,60 +1,83 @@
 
 
- 
-  
+
+
 // Defining async function
 async function getapi(url) {
     // Storing response
     let response = await fetch(url);
-    
-    
+
+
     // Storing data in form of JSON
     var data = await response.json();
     console.log(data);
-    
+
     show(data);
 }
 // Calling that async function
 
-function getname(){
-    
-    
-    var api_url ="http://localhost:3000/api/v1/movies/";
-    let name=document.getElementById("name").value;
-    console.log(name)
-    if (name!=""){
+function getname() {
 
-        api_url+="?name="
+
+    let api_url = "http://localhost:3000/api/v1/movies/";
+    let name = document.getElementById("name").value;
+
+    if (name !== "") {
+
+        api_url += "?name="
     }
-    api_url+=name
-    console.log(api_url)
-    
+    api_url += name
+
     getapi(api_url);
 }
-  
- 
+
+
 // Function to define innerHTML for HTML table
 function show(data) {
-    let tab = 
+    let tab =
         `<thead class="table-dark"><tr>
           <th scope="col">ID</th>
           <th scope="col">original_title</th>
           <th scope="col">genres</th>
           <th scope="col">production_companies</th>
-         </tr></thead><tbody>`;
-    
-    // Loop to access all rows 
-    for (let r of data.data) {
-        tab += `<tr> 
-    <th scope="row">${r.id} </th>
-    <td>${r.original_title}</td>
-     <td>${r.genres}</td> 
-     <td>${r.production_companies}</td>          
-</tr>`;
+         </tr></thead>`;
 
-// document.getElementById("employees").innerHTML = r;
+    // Loop to access all rows 
+    if (data.data !== null) {
+
+        document.getElementById("employees").innerHTML = tab;
+        const tbody = document.createElement("tbody");
+        let x = document.getElementById("employees");
+        for (let r of data.data) {
+            //         tab += `<tr> 
+            //     <th scope="row">${r.id} </th>
+            //     <td>${r.original_title}</td>
+            //      <td>${r.genres}</td> 
+            //      <td>${r.runtime}</td>          
+            // </tr>`;
+            const tr = document.createElement("tr");
+            const th = document.createElement("th");
+            const td2 = document.createElement("td");
+            const td3 = document.createElement("td");
+            const td4 = document.createElement("td");
+            // element.classList.add("my-class");
+            th.scope="row";
+            
+            th.textContent = r.id;
+            tr.appendChild(th);
+            td2.textContent = r.original_title;
+            tr.appendChild(td2);
+            td3.textContent = r.genres;
+            tr.appendChild(td3);
+            td4.textContent = r.production_companies;
+            tr.appendChild(td4);
+            
+            tbody.appendChild(tr);
+
+
+        }
+        x.appendChild(tbody);
     }
-    tab+=`</tbody>`
+    tab += `</tbody>`
     // Setting innerHTML as tab variable
-    document.getElementById("employees").innerHTML = tab;
 }
